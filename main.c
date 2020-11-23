2893de579e9e32b529887db2d77dcbacbc21ee58
David Nacarino <d_nacarino@yahoo.com>
2020-11-22 23:50:12 -0500

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "string.h"
+
+int main(int argc, char const *argv[]) {
+uint8_t myVal = 0;
+uint8_t myConst = 0;
+
+
+for(;;)
+{
+  if(myVal > 5){
+    myVal = 0;
+    myConst++;
+  }else
+  myVal++;
+}
+
+  return 0;
+}
